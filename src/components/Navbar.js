75aa58ce7e1f27bcd31555f455a6cdf5bf5a6476
Javier Bartús Gómez <91955122+Bartu10@users.noreply.github.com>
2024-03-16 10:42:10 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Bartu10/JavierBartusPorfolio"
+                href="https://github.com/Bartu10/JavierBartus"
                 target="_blank"
                 className="fork-btn-inner"
               >
